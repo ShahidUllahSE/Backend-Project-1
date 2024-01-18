4b18f7579f1e8f031ac2d0f308d3ee0d183702f6
ShahidUllahSE <shahidbangash8899@gmail.com>
2024-01-18 12:14:18 +0500

    fetched oroducts data

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { NotificationManager } from 'react-notifications'
+import { useNavigate } from 'react-router'
 
 const Logout = () => {
+    const navigate= useNavigate();
+    useEffect(()=>{
+        const token = localStorage.getItem("token")
+        if(!token){
+            NotificationManager.error("Already logged out ", "" , 3000)
+            navigate("/home")
+        } 
+        else{
+            localStorage.removeItem("token")
+            NotificationManager.warning("Successfully logged out ", "" , 3000)
+            navigate("/home")
+        }
+    })
   return (
     <div>
       logout
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,34 @@
+import axios from 'axios';
 import React from 'react'
-
+import { useEffect, useState } from 'react'
 const Products = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+        axios.get("http://localhost:3001/api/products")
+
+        .then(res => {
+
+            setProducts(res.data)
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    },[products])
   return (
     <div>
-      products
+      
+        {products.length>0 && products.map(prod => {
+            return(
+                <tr>
+                    <td>{prod.title}</td>
+                    <td>{prod.price}</td>
+                </tr>
+            )
+        })}
+
     </div>
   )
 }
